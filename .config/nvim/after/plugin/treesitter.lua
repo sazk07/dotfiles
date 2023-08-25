a91e3849e16ba8f2d158d2616eb2e06ec17f318a
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2023-08-26 03:15:52 +0500

    Update treesitter setup

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -29,6 +29,23 @@ require'nvim-treesitter.configs'.setup {
         ["<leader>I"] = "@parameter.inner",
       },
     },
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region"},
+        ["as"] = { query = "@scope", query_group = "locals", desc = "select language scope"},
+      },
+      selection_modes = {
+        ['@parameter.outer'] = 'v',
+        ['@function.outer'] = 'V',
+        ['@class.outer'] = '<c-v>',
+      },
+      include_surrounding_whitespace = true,
+    },
   },
   autotag = {
     enable=true,

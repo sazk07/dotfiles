c4f2a501da6356803d16cf683e201763dd2a2a7f
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2023-09-02 03:05:12 +0500

    Update telescope and treesitter configs

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,4 @@
-require('telescope').setup{
+require('telescope').setup {
   extensions = {
     fzf = {
       fuzzy = true,
@@ -11,8 +11,6 @@ require('telescope').setup{
 require('telescope').load_extension('fzf')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})

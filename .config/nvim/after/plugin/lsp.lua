cb31bdd175f6e992bfd4878c6e53d9a736c137f7
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2023-10-01 18:34:53 +0500

    Fix keybindings

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -19,9 +19,12 @@ lsp.nvim_workspace()
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_action = require('lsp-zero').cmp_action()
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+  ['<C-b>'] = cmp_action.luasnip_jump_backward(),
 	['<C-Space>'] = cmp.mapping.complete(),
 })
 

1229c622436ef44046a79edf32d2e814c2049296
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2024-02-12 04:08:37 +0500

    feat: Disable global md as part of vimwiki

--- config/nvim/lua/custom/vimwiki.lua
+++ config/nvim/lua/custom/vimwiki.lua
@@ -3,3 +3,7 @@ let g:vimwiki_list = [{"path":"~/unodrive/vimwiki/", "path_html":"~/public_html/
   ]])
 
 -- vim.g.vimwiki_list = { { path = "~/unodrive/vimwiki/" }, { path_html = "~/public_html/" } }
+
+vim.cmd([[
+let g:vimwiki_global_ext = 0
+  ]])

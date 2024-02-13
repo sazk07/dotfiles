5c4255a5571fb1d5e5642e54b4006823454107c5
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2024-02-13 18:59:43 +0500

    feat: Edit vimwiki to markdown

--- config/nvim/lua/custom/vimwiki.lua
+++ config/nvim/lua/custom/vimwiki.lua
@@ -1,5 +1,9 @@
+-- vim.cmd([[
+-- let g:vimwiki_list = [{"path":"~/unodrive/vimwiki/", "path_html":"~/public_html/"}]
+--   ]])
+
 vim.cmd([[
-let g:vimwiki_list = [{"path":"~/unodrive/vimwiki/", "path_html":"~/public_html/"}]
+let g:vimwiki_list = [{"path":"~/unodrive/vimwiki/", "syntax": "markdown", "ext": ".md"}]
   ]])
 
 -- vim.g.vimwiki_list = { { path = "~/unodrive/vimwiki/" }, { path_html = "~/public_html/" } }
@@ -7,3 +11,7 @@ let g:vimwiki_list = [{"path":"~/unodrive/vimwiki/", "path_html":"~/public_html/
 vim.cmd([[
 let g:vimwiki_global_ext = 0
   ]])
+
+vim.cmd([[
+let g:vimwiki_ext2syntax = {}
+]])

294c09a53860b716b50d111c4f78144bdf4b876b
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2024-09-12 07:23:22 +0500

    remove htmx lsp from config. Change path for vimwiki. Change path for codium startup under arch i3

--- config/nvim/lua/custom/vimwiki.lua
+++ config/nvim/lua/custom/vimwiki.lua
@@ -3,7 +3,7 @@
 --   ]])
 
 vim.cmd([[
-let g:vimwiki_list = [{"path":"~/unodrive/vimwiki/", "syntax": "markdown", "ext": ".md"}]
+let g:vimwiki_list = [{"path":"~/Documents/unodrive/vimwiki/", "syntax": "markdown", "ext": ".md"}]
   ]])
 
 -- vim.g.vimwiki_list = { { path = "~/unodrive/vimwiki/" }, { path_html = "~/public_html/" } }

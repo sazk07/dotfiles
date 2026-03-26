9661d683bff8aaa7f2ecfe9511e86cda2da7c476
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2026-03-27 00:47:11 +0500

    Edit telescope ver

--- config/nvim/lua/core/packer.lua
+++ config/nvim/lua/core/packer.lua
@@ -23,7 +23,7 @@ return require("packer").startup(function(use)
 	use({
 		{
 			"nvim-telescope/telescope.nvim",
-			tag = "0.1.8",
+			tag = "*",
 			-- or                            , branch = '0.1.x',
 			requires = {
 				"nvim-lua/plenary.nvim",
